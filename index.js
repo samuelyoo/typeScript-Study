51aef7357606c2389f2ad65dda61139ec242311e
kuro sammy <sam.hc.yoo@gmail.com>
2020-08-17 22:59:55 -0400

    ? exmaple

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Sanny", age = 15, gender = "male";
+const myInfo = (name, age, gender) => {
+    console.log(`Hello ${name}, I am ${age}, and ${gender}`);
+};
+myInfo(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+const name = "Sanny",
+    age = 15,
+    gender = "male";
+
+
+// ? can make it optional in typescirpt
+const myInfo = (name, age, gender?) =>{
+    console.log( `Hello ${name}, I am ${age}, and ${gender}`);
+};
+
+myInfo(name, age)
+
+export {};
