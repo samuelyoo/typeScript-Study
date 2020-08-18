d7aac8e3abd350b51baa20149b1f7ea5d41a3700
kuro sammy <sam.hc.yoo@gmail.com>
2020-08-17 23:31:37 -0400

    added interface

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const myInfo = (name, age, gender) => {
-    return `Hello ${name}, I am ${age}, and ${gender}`;
-};
-console.log(myInfo("name", 23, "gender"));
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-const myInfo = (name: string, age: number, gender: string): string =>{
-    return `Hello ${name}, I am ${age}, and ${gender}`;
+// const myInfo = (name: string, age: number, gender: string): string =>{
+//     return `Hello ${name}, I am ${age}, and ${gender}`;
+// };
+
+// console.log(myInfo("name", 23, "gendeeeer"));
+
+// export {};
+
+// interface not converting to js file
+interface Human{
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const coder ={
+    name: "sammy",
+    age: 15,
+    gender: "male"
+}
+
+const myInfo = (coder: Human): string =>{
+    return `Hello ${coder.name}, I am ${coder.age}, and ${coder.gender}`;
 };
 
-console.log(myInfo("name", 23, "gendeeeer"));
+console.log(myInfo(coder));
 
 export {};
