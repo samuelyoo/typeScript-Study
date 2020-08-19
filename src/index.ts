f1e66eb7d0a662214dcf6cd50c8a0acca447d309
kuro sammy <sam.hc.yoo@gmail.com>
2020-08-18 20:26:10 -0400

    added class instead of interface

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,36 @@
 
 // export {};
 
-// interface not converting to js file
-interface Human{
-    name: string;
-    age: number;
-    gender: string;
+// // interface not converting to js file
+// interface Human{
+//     name: string;
+//     age: number;
+//     gender: string;
+// }
+
+class Human{
+    public name: string;
+    private age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const coder ={
-    name: "sammy",
-    age: 15,
-    gender: "male"
-}
+// const coder ={
+//     name: "sammy",
+//     age: 15,
+//     gender: "male"
+// }
+
+const student = new Human("Samuel", 16, "male")
 
 const myInfo = (coder: Human): string =>{
     return `Hello ${coder.name}, I am ${coder.age}, and ${coder.gender}`;
 };
 
-console.log(myInfo(coder));
+console.log(myInfo(student));
 
 export {};
